cac14e662d42c755aba56da1ef866fdcf8205658
Bakri Diyaolu <bakridiyaolu@gmail.com>
2024-07-27 21:27:29 -0500

    analytics commit

--- Smart_Meal.py
+++ Smart_Meal.py
@@ -57,7 +57,7 @@ GA_JS = f"""
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-VJM1HSFC4J"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
+  function gtag(){{dataLayer.push(arguments);}}
   gtag('js', new Date());
 
   gtag('config', 'G-VJM1HSFC4J');
